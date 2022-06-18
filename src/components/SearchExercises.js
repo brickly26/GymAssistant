7fd7b30e14d64f7f04eb2b91fdfd8beae6c100e5
brickly26 <sabburak26@gmail.com>
2022-06-18 13:13:13 -0400

    created useEffect hook to show a list of bodyparts when component loads

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,9 +4,25 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
+import HorizontalScrollbar from "./HorizontalScrollbar";
+
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
   const [exercises, setExercises] = useState([]);
+  const[bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts(['all', ...bodyPartsData])
+    };
+
+    fetchExercisesData();
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
@@ -15,13 +31,15 @@ const SearchExercises = () => {
         exerciseOptions
       );
 
-      const searchedExercises = exercisesData.filter((exercise) => exercise.name.toLowerCase().includes(search)
-        || exercise.target.toLowerCase().includes(search)
-        || exercise.equipment.toLowerCase().includes(search)
-        || exercise.bodyPart.toLowerCase().includes(search)
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
       );
 
-      setSearch('');
+      setSearch("");
       setExercises(searchedExercises);
     }
   };
@@ -34,7 +52,7 @@ const SearchExercises = () => {
         mb="50px"
         textAlign="center"
       >
-        Aweseom Exercises You <br />
+        Awesome Exercises You <br />
         Should Know
       </Typography>
       <Box position="relative" mb="72px">
@@ -67,6 +85,9 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ postion: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts}/>
       </Box>
     </Stack>
   );
